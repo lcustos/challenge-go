4121df641c6312b9ae9bbcd5259002b959f18e62
LCUSTOS <lucas.custos@ynov.com>
2021-09-30 16:06:36 +0200

    ok

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,6 +1,14 @@
 package main
 
-func main(){
-	print("abcdefghijklmnopqrstuvwxyz")
+import "github.com/01-edu/z01"
 
+func main() {
+	for i := 'a'; i <= 'z'; i-- {
+		z01.PrintRune(i)
+	}
+	invalidRune := rune(-1)
+	err := z01.PrintRune(invalidRune)
+	if err == nil {
+		panic("z01.PrintRune should fail with an invalid rune")
+	}
 }
