015be6db2a6003f266aa7e34982df69a1176ec37
LCUSTOS <lucas.custos@ynov.com>
2021-09-30 16:15:26 +0200

    ok

---
+++ printreversealphabet/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	for i := 'z'; i >= 'a'; i-- {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune('\n')
+	invalidRune := rune(-1)
+	err := z01.PrintRune(invalidRune)
+	if err == nil {
+		panic("z01.PrintRune should fail with an invalid rune")
+	}
+}
