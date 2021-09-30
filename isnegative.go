012daa7a39b7133451feb17734929dafa4f39249
LCUSTOS <lucas.custos@ynov.com>
2021-09-30 17:08:39 +0200

    ok

--- isnegative.go
+++ isnegative.go
@@ -3,11 +3,11 @@ package tls_challenge_go_21_22
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	if nb >= 0 {
-		z01.PrintRune('F')
-
-	} else if nb < 0 {
+	if nb < 0 {
 		z01.PrintRune('T')
+
+	} else {
+		z01.PrintRune('F')
 
 	}
 	z01.PrintRune('\n')
