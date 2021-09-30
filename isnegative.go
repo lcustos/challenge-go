8d8a8e9df91828742429adaff9ee722bfb23250e
LCUSTOS <lucas.custos@ynov.com>
2021-09-30 17:05:47 +0200

    ok

--- isnegative.go
+++ isnegative.go
@@ -1,14 +1,14 @@
-package piscine
+package tls_challenge_go_21_22
 
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb >= 0 {
 		z01.PrintRune('F')
-		z01.PrintRune('\n')
+
 	} else if nb < 0 {
 		z01.PrintRune('T')
-		z01.PrintRune('\n')
-	}
 
+	}
+	z01.PrintRune('\n')
 }
