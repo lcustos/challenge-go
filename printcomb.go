f00f6036442c14d6c70890b52a26d8952c7b5f17
LCUSTOS <lucas.custos@ynov.com>
2021-10-01 11:30:59 +0200

    ok

--- printcomb.go
+++ printcomb.go
@@ -3,24 +3,27 @@ package tls_challenge_go_21_22
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := '0'; i <= '7'; i++ {
-		for j := '0'; j <= '8'; j++ {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				if i < j {
 					if j < k {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(k)
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
+						if i != '7' || j != '8' || k != '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 
 					}
 
 				}
 
 			}
-
 		}
 
 	}
+	z01.PrintRune('$')
+	z01.PrintRune('\n')
 }
