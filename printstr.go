dbeaa977c1de816586121f60a6e34acbcee3c39a
LCUSTOS <lucas.custos@ynov.com>
2021-10-04 16:17:41 +0200

    ok

--- printstr.go
+++ printstr.go
@@ -2,8 +2,8 @@ package tls_challenge_go_21_22
 
 import "github.com/01-edu/z01"
 
-func PrintStr(str string) {
-	for _, char := range str {
+func PrintStr(s string) {
+	for _, char := range s {
 		_ = z01.PrintRune(char)
 	}
 }
---
+++ strlen.go
@@ -0,0 +1,10 @@
+package tls_challenge_go_21_22
+
+func StrLen(s string) int {
+	n := 0
+	for i, _ := range s {
+		i++
+		n++
+	}
+	return n
+}
