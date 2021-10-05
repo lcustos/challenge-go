7d9eb20a2528fa2764c3fde8eea54b451205f6e8
LCUSTOS <lcustos@noreply.git.ytrack.learn.ynov.com>
2021-10-05 13:28:13 +0000

    Update 'sortintegertable.go'

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,6 +1,5 @@
 package tls_challenge_go_21_22
 
-import "fmt"
 
 func SortIntegerTable(table []int) {
 	i := 1
@@ -12,5 +11,4 @@ func SortIntegerTable(table []int) {
 			i++
 		}
 	}
-	fmt.Println(table)
 }
