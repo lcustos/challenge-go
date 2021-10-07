fd6cd3e9ca75c24653e8c9471b5f216e72a2ff72
LCUSTOS <lucas.custos@ynov.com>
2021-10-07 14:30:36 +0200

    ok

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,7 +2,7 @@ package tls_challenge_go_21_22
 
 func IterativeFactorial(nb int) int {
 	f := 1
-	if nb < 0 || nb >= 9223372036854775807 {
+	if nb < 0 || nb >= 922337203685477 {
 		return 0
 	} else {
 		for i := 1; i <= nb; i++ {
