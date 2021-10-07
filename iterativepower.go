5c734d904b66a992ee6efe712d08a7c8666d9d57
LCUSTOS <lucas.custos@ynov.com>
2021-10-07 14:44:17 +0200

    ok

--- iterativepower.go
+++ iterativepower.go
@@ -1,13 +1,10 @@
 package tls_challenge_go_21_22
 
 func IterativePower(nb int, power int) int {
-	if nb > 25 {
+	if nb > 25 || power > 25 || power <= 0 {
 		return 0
 	}
 	n := nb
-	if power == 0 {
-		return 0
-	}
 	for i := 1; i < power; i++ {
 		nb = nb * n
 	}
