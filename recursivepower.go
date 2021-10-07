f6dacf904e5ab1a78d1f32d7302fb1887c6dc09b
LCUSTOS <lucas.custos@ynov.com>
2021-10-07 15:02:51 +0200

    ok

---
+++ recursivepower.go
@@ -0,0 +1,12 @@
+package tls_challenge_go_21_22
+
+func RecursivePower(nb int, power int) int {
+	if power == 0 {
+		return 1
+	} else if nb > 25 || power > 25 || power <= 0 {
+	}
+	return 0
+	{
+		return nb * RecursivePower(nb, power-1)
+	}
+}
