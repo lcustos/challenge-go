c9defbc5789dd65d76752a08e0444c0cdd688829
LCUSTOS <lucas.custos@ynov.com>
2021-10-11 15:05:33 +0200

    ok

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package tls_challenge_go_21_22
 func IsPrintable(s string) bool {
 	IU := true
 	for i := 0; i <= len(s)-1; i++ {
-		if rune(s[i]) >= 'A' && rune(s[i]) <= 'Z' || rune(s[i]) >= 'a' && rune(s[i]) <= 'z' || rune(s[i]) >= '0' && rune(s[i]) <= '9' {
+		if rune(s[i]) >= 32 && rune(s[i]) <= 126 {
 			IU = true
 		} else {
 			return false
