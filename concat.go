fac7660ff68859bf5ab7ecb3b29c8bc8d51c0c73
LCUSTOS <lucas.custos@ynov.com>
2021-10-11 14:24:18 +0200

    ok

---
+++ concat.go
@@ -0,0 +1,5 @@
+package tls_challenge_go_21_22
+
+func Concat(str1 string, str2 string) string {
+	return (str1 + str2)
+}
--- test/main.go
+++ test/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	tls_challenge_go_21_22 "challenge-go"
+	piscine "challenge-go"
 	"fmt"
 )
 
 func main() {
-	fmt.Println(tls_challenge_go_21_22.IsPrime(847339))
+	fmt.Println(piscine.Index("Hello!", "l"))
+	fmt.Println(piscine.Index("Salut!", "alu"))
+	fmt.Println(piscine.Index("Ola!", "hOl"))
 }
