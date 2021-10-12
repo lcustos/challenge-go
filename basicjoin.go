c0c78c24f8538f88d36cc253365d6ac535d65b1c
LCUSTOS <lucas.custos@ynov.com>
2021-10-12 09:33:52 +0200

    ok

--- basicjoin.go
+++ basicjoin.go
@@ -1,11 +1,8 @@
 package tls_challenge_go_21_22
 
-import "fmt"
-
 func BasicJoin(elems []string) string {
 	concat := ""
 	for _, word := range elems {
-		fmt.Println(word)
 		concat += word
 	}
 	return concat
--- join.go
+++ join.go
@@ -2,9 +2,13 @@ package tls_challenge_go_21_22
 
 func Join(strs []string, sep string) string {
 	concat := ""
-	for _, word := range strs {
+	for i, word := range strs {
 		concat += word
-		concat += sep
+		i++
+		if i < len(strs) {
+			concat += sep
+		}
+
 	}
 	return concat
 }
