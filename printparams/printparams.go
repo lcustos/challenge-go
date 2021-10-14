c928aa9b93ed8e7b67c4f4e2d04ec32dcb3bf56b
LCUSTOS <lucas.custos@ynov.com>
2021-10-14 16:13:57 +0200

    ok

---
+++ printparams/printparams.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+
+	"os"
+)
+
+func main() {
+	for _, element := range os.Args[1:] {
+		for _, word := range element {
+			z01.PrintRune(word)
+		}
+
+		z01.PrintRune('\n')
+	}
+}
