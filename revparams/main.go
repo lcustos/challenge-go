a7aade1bdff854ec3c0023af29742ba784ee7373
LCUSTOS <lucas.custos@ynov.com>
2021-10-14 14:59:39 +0200

    ok

---
+++ revparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	len := 0
+	for i := range arguments {
+		len = i
+	}
+	for i := len; i >= 1; i-- {
+		for _, k := range arguments[i] {
+			z01.PrintRune(k)
+		}
+		z01.PrintRune(10)
+	}
+}
