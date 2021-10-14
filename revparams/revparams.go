69ce892d4e4b87fc08beb27c48cc9a95b83f1b77
LCUSTOS <lucas.custos@ynov.com>
2021-10-14 16:29:28 +0200

    ok

--- revparams/revparams.go
+++ revparams/revparams.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	len := 0
+	for i := range os.Args {
+		len = i
+	}
+	for i := len; i >= 1; i-- {
+		for _, k := range os.Args[i] {
+			z01.PrintRune(k)
+		}
+		z01.PrintRune('\n')
+	}
 }
