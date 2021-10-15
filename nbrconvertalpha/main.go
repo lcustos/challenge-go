d71e11be0a252dd5127645caf349745225069302
LCUSTOS <lucas.custos@ynov.com>
2021-10-15 09:30:11 +0200

    ok

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	arg := os.Args[1:]
+	flag := false
+	for _, val := range arg {
+		if val == "--upper" {
+			flag = true
+		}
+	}
+
+	if flag {
+		for i := 0; i <= len(arg)-1; i++ {
+			z01.PrintRune(ToUpper(ConvertToLetter(arg[i])))
+		}
+		z01.PrintRune(10)
+	} else if len(arg)-1 == 0 {
+		z01.PrintRune(10)
+	} else {
+		for i := 0; i <= len(arg)-1; i++ {
+			z01.PrintRune(ConvertToLetter(arg[i]))
+		}
+		z01.PrintRune(10)
+	}
+
+}
+
+func ConvertToLetter(s string) rune {
+	numb := 0
+	for _, i := range s {
+		count := 0
+		for k := '0'; k < i; k++ {
+			count++
+		}
+		numb = numb*10 + count
+	}
+	numb = numb + 96
+	if numb >= 123 {
+		return ' '
+	}
+	return rune(numb)
+}
+
+func ToUpper(r rune) rune {
+	if r-32 < 65 {
+		return r
+	}
+	return r - 32
+}
