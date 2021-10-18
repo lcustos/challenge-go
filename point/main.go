d412ef74721b425bcaa28be27cf6675506d7c6dc
LCUSTOS <lucas.custos@ynov.com>
2021-10-18 14:57:06 +0200

    ok

--- point/main.go
+++ point/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 func setPoint(x *int, y *int) {
 	*x = 42
@@ -11,5 +13,40 @@ func main() {
 	x := 0
 	y := 0
 	setPoint(&x, &y)
-	fmt.Printf("x = %d, y = %d", x, y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	PrintNbr(x)
+	z01.PrintRune(' ')
+	z01.PrintRune(',')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	PrintNbr(y)
+}
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		if n/10 != 0 {
+			PrintNbr(n / -10)
+		}
+		mod := '0'
+		for i := 0; i < -(n % 10); i++ {
+			mod++
+		}
+		z01.PrintRune(mod)
+	} else if n == 0 {
+		z01.PrintRune('0')
+	} else {
+		if n/10 != 0 {
+			PrintNbr(n / 10)
+		}
+		mod := '0'
+		for i := 0; i < n%10; i++ {
+			mod++
+		}
+		z01.PrintRune(mod)
+	}
 }
