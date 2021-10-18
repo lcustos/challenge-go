b40f525db21b2afc417a88860f49ae3957f8f11c
LCUSTOS <lucas.custos@ynov.com>
2021-10-18 11:04:26 +0200

    ok

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,8 +3,11 @@ package tls_challenge_go_21_22
 func SplitWhiteSpaces(s string) []string {
 	space := 0
 	len2 := 0
-	for _, i := range s {
-		if i == ' ' || i == '\t' || i == '\n' {
+	for i := 0; i < len(s); i++ {
+		if s[i] == ' ' || s[i] == '\t' || s[i] == '\n' {
+			if s[i-1] == ' ' || s[i-1] == '\t' || s[i-1] == '\n' {
+				space--
+			}
 			space++
 		}
 	}
