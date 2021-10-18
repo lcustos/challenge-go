1df6f6956cdecf35a756a6e647289d1cda235541
LCUSTOS <lucas.custos@ynov.com>
2021-10-18 08:40:11 +0200

    ok

--- appendrange.go
+++ appendrange.go
@@ -1,8 +1,8 @@
 package tls_challenge_go_21_22
 
 func AppendRange(min, max int) []int {
-	Range := []int{}
-	if min > max {
+	Range := []int(nil)
+	if min >= max {
 		return Range
 	} else {
 		for i := min; i < max; i++ {
