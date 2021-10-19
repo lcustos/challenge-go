21ebc8ed6955460f14bb120d18d81b31d1a214e1
LCUSTOS <lucas.custos@ynov.com>
2021-10-19 14:10:43 +0200

    ok

--- flags/main.go
+++ flags/main.go
@@ -1,37 +1,101 @@
 package main
 
 import (
-	"os"
-
+	"fmt"
 	"github.com/01-edu/z01"
+	"os"
 )
 
 func main() {
-	for _, word := range os.Args {
-		if word == "--help" || word == "--h" || len(os.Args) == 0 {
-		}
-		if word == "--insert" || word == "--i" {
-			for _, element := range os.Args[1:] {
-				for _, word1 := range element {
-					z01.PrintRune(word1)
-				}
-				z01.PrintRune('\n')
+	args := os.Args
+	args = args[1:]
+	len := 0
+	for i := range args {
+		len = i + 1
+	}
+	if len == 0 || args[0] == "-h" || args[0] == "--help" {
+		fmt.Print("--insert\n  -i\n\t This flag inserts the string into the string passed as argument.\n--order\n  -o\n\t This flag will behave like a boolean, if it is called it will order the argument.\n")
+	} else {
+		var runes []rune
+		order := false
+		order_prev := false
+		insert := false
+		for _, arg := range args {
+			length := 0
+			for j := range arg {
+				length = j + 1
 			}
-		}
-		if word == "--order" {
-			for i := 1; i <= len(os.Args)-1; i++ {
-				for j := i + 1; j <= len(os.Args)-1; j++ {
-					if os.Args[i] > os.Args[j] {
-						os.Args[i], os.Args[j] = os.Args[j], os.Args[i]
+			if length > 9 && arg[:9] == "--insert=" {
+				runes_temp := []rune(arg[9:])
+
+				for i := 0; i < length-9; i++ {
+					runes = append(runes, runes_temp[i])
+				}
+				insert = true
+
+			} else if length > 3 && arg[:3] == "-i=" {
+				runes_temp := []rune(arg[3:])
+
+				for i := 0; i < length-3; i++ {
+					runes = append(runes, runes_temp[i])
+				}
+				insert = true
+
+			} else if (length == 7 && arg[:7] == "--order") || (length == 2 && arg[:2] == "-o") {
+				order = true
+				order_prev = true
+				continue
+			} else if order_prev {
+				runes_temp := []rune(arg)
+				for i := range runes_temp {
+					runes = append(runes, runes_temp[i])
+				}
+			} else {
+				runes_print := []rune(arg)
+				if order {
+					for i := range runes_print {
+						runes = append(runes, runes_print[i])
 					}
 				}
-			}
-			for _, element := range os.Args[1:] {
-				for _, word1 := range element {
-					z01.PrintRune(word1)
+				for i := range runes_print {
+					z01.PrintRune(runes_print[i])
 				}
-				z01.PrintRune('\n')
 			}
+			order_prev = false
+
+		}
+		if order {
+			Order(runes)
+		}
+
+		if order || insert {
+			for i := range runes {
+				z01.PrintRune(runes[i])
+			}
+		}
+
+		z01.PrintRune(10)
+	}
+
+}
+
+func Order(array []rune) {
+	len := 0
+	for index := range array {
+		len = index + 1
+	}
+
+	sorted := false
+	for !sorted {
+		count := 0
+		for i := 1; i < len; i++ {
+			if array[i] < array[i-1] {
+				array[i], array[i-1] = array[i-1], array[i]
+				count++
+			}
+		}
+		if count == 0 {
+			sorted = true
 		}
 	}
 }
