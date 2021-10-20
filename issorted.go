b24999ce3d96c63af0b4ab859ae5e081617793ac
LCUSTOS <lucas.custos@ynov.com>
2021-10-20 14:32:36 +0200

    ok

--- issorted.go
+++ issorted.go
@@ -3,7 +3,7 @@ package tls_challenge_go_21_22
 func IsSorted(f func(a, b int) int, a []int) bool {
 	verif := []bool{}
 	for i := 1; i < len(a); i++ {
-		if f(a[i-1], a[i]) < 0 {
+		if f(a[i-1], a[i]) <= 0 {
 			verif = append(verif, true)
 		} else if f(a[i-1], a[i]) > 0 {
 			verif = append(verif, false)
