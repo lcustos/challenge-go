a1d605df2ae25d4000eb0e23462cc662a14b81ae
LCUSTOS <lucas.custos@ynov.com>
2021-10-20 14:16:36 +0200

    ok

--- issorted.go
+++ issorted.go
@@ -2,22 +2,24 @@ package tls_challenge_go_21_22
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	s := false
-	for i := 1; i < len(a); i++ {
-		if a[0] < a[1] {
+	if a[0] < a[1] {
+		for i := 1; i < len(a); i++ {
 			if f(a[i-1], a[i]) > 0 {
 				s = true
 			} else if f(a[i-1], a[i]) <= 0 {
-				return false
 			}
-		} else if a[0] > a[1] {
+			return false
+		}
+	} else if a[0] > a[1] {
+		for i := 1; i < len(a); i++ {
 			if f(a[i-1], a[i]) < 0 {
 				s = true
 			} else if f(a[i-1], a[i]) >= 0 {
 				return false
 			}
-		} else {
-			return false
 		}
+	} else {
+		return false
 	}
 	return s
 }
